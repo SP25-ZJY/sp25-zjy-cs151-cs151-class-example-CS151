e576538f447f9839fe1f5b157cdb57bbe31cf0de
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-01-10 15:38:57 -0500

    Note 3 is added

---
+++ note_3_teaspoon.py
@@ -0,0 +1,9 @@
+#Prompt for number of tablespoons
+tbsp_str = input("Enter the number of tablespoons:")
+tbsp_int = int(tbsp_str)
+
+#Do conversion of 3 tsp in a tbsp
+teaspoons = tbsp_int * 3
+
+#Output the result
+print("You need this many teaspoons:", teaspoons) 
